bbd0fb10a6d7986b30249234b0f47ec64be011be
Tolu Abbass <abbasstolulope@gmail.com>
2024-12-29 23:07:40 +0000

    changed about me section

--- src/sections/AboutMe/AboutMe.jsx
+++ src/sections/AboutMe/AboutMe.jsx
@@ -20,7 +20,7 @@ const AboutMe = () => {
                 <SectionHeading title={'About Me'}/>
             </Box>
 
-            <Box sx={{display: 'flex', justifyContent: 'space-between', width: '100%'}}>
+            <Box sx={{display: 'flex', justifyContent: 'space-between', width: '100%', alignItems: 'center', flexDirection: { xs: 'column', sm: 'row' }}}>
                 <Box sx={{width: '100%', height: '25rem', overflow: 'hidden', padding: 0}}>
                     <img
                         src={'/images/portfolio-headshot.png'} // Relative path from the public folder
@@ -29,18 +29,10 @@ const AboutMe = () => {
                     />
                 </Box>
 
-                <Box sx={{width: '75%'}}>
+                <Box sx={{width: '75%', display: 'flex', flexDirection: 'column', justifyContent: 'center'}}>
                     <Typography>
                         Hello! I'm a 23-year-old Software Engineer based in London, with a passion for building scalable and impactful software applications.
                         Currently, I am part of Avanade, a joint venture between Microsoft and Accenture, where I contribute to the development of large-scale software solutions.
-                        <br></br>
-                        <br></br>
-                        I'm experienced with Azure Cloud Services, holding two Microsoft certifications and actively working toward my third. My expertise in cloud 
-                        technologies complements my software engineering background, enabling me to design and deliver efficient, cloud-enabled solutions.
-                        <br></br>
-                        <br></br>
-                        In addition to my professional work, I also take on freelance projects, specializing in creating engaging and user-friendly websites
-                        using platforms like Webflow and Wix Studios. This blend of corporate and freelance experience has honed my technical skills and creative problem-solving abilities.
                         <br></br>
                         <br></br>
                         When I'm not coding, I'm continually exploring new technologies and striving to grow both personally and professionally. Let's create something amazing together!
